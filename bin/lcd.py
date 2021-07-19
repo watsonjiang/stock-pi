55cd242332c90a5e298a6e90571788d5fb1a25a9
watson <jiangq@xiaopeng.com>
2021-07-19 23:40:04 +0800

    update

--- bin/lcd.py
+++ bin/lcd.py
@@ -32,6 +32,15 @@ def print_char612_xy(x, y, c):
     b.i2c_rdwr(i2c_msg.write(0x00, [0x20, x, y]),
               i2c_msg.write(0x00, [0x25, c, 0x00]))
 
+def print_char1212_xy(x, y, c):
+    ''' x 0~7 y 0~3
+    '''
+    px = x * 15
+    py = y * 15
+    data = [0x25] + c.encode('gb2312') + [0x00]
+    b.i2c_rdwr(i2c_msg.write(0x00, [0x20, x, y]),
+              i2c_msg.write(0x00, data))
+
 
 for i in range(0, 30):
   time.sleep(1)
@@ -39,3 +48,13 @@ for i in range(0, 30):
   y = i // 18
   print('move to ', x, y, 'write ', chr(0x30+i))
   print_char612_xy(x, y, 0x30+i)
+
+
+text = "中华人民共和国"
+for i in text:
+  time.sleep(1)
+  x = i % 7
+  y = i // 7 + 2
+  print('move to ', x, y, 'write ', i)
+  print_char1212_xy(x, y, i)
+
