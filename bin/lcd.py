e00615af01f61b36a4af7d2a0b29c5ba8c6e7cb4
watson <jiangq@xiaopeng.com>
2021-07-19 23:23:38 +0800

    update

--- bin/lcd.py
+++ bin/lcd.py
@@ -16,22 +16,26 @@ print('switch light ', 0xff)
 b.write_byte_data(0x00, 0x13, 0xff)
 
 
-def print_char_xy(x, y, c):
-   msg1 = i2c_msg()
-   msg1.write(0x00, [0x20, x, y]) 
-   msg2 = i2c_msg()
-   msg2.write(0x00, [0x24, c, 0x00])
-   b.i2c_rdwr(msg1, msg2)
+def print_char57_xy(x, y, c):
+    ''' x 0~20 y 0~7
+    ''' 
+    px = x * 6
+    py = y * 8
+    b.i2c_rdwr(i2c_msg.write(0x00, [0x20, px, py]),
+              i2c_msg.write(0x00, [0x24, c, 0x00]))
 
-j = 0
-for i in range(0, 62):
+def print_char612_xy(x, y, c):
+    ''' x 0~17  y 0~3
+    '''
+    px = x * 7
+    py = y * 15
+    b.i2c_rdwr(i2c_msg.write(0x00, [0x20, x, y]),
+              i2c_msg.write(0x00, [0x25, c, 0x00]))
+
+
+for i in range(0, 30):
   time.sleep(1)
-  #x = 5 * (i % 25) + 1
-  x = 10 * (i % 3) + 1
-  #y = 7 * (i // 25) + 1
-  y = 10 * (i % 3) + 1
-  print('move to ', x, y, 'write ', chr(0x30+j))
-  print_char_xy(x, y, 0x30+j)
-  j = j + 1
-  if j == ord('z'):
-     j == 0
+  x = i % 18
+  y = i // 18
+  print('move to ', x, y, 'write ', chr(0x30+i))
+  print_char612_xy(x, y, chr('A')+i)
