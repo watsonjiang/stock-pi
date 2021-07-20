af8f887a89f5c675da35f342b932f26170027cb9
watson <jiangq@xiaopeng.com>
2021-07-20 23:40:59 +0800

    update

--- bin/lcd.py
+++ bin/lcd.py
@@ -12,14 +12,24 @@ print('clear screen')
 lcd.clear()
 lcd.brightness(200)
 
-time.sleep(1)
-text = "0123456789abcdefghijklmnopqrstuvwxyz你好"
-lcd.print_str8_xy(0, 0, text)
+#time.sleep(1)
+#text = "0123456789abcdefghijklmnopqrstuvwxyz"
+#lcd.print_str8_xy(0, 0, text)
 
-time.sleep(1)
-text = "中华人民共和国"
-lcd.print_str12_xy(0, 1, text)
+#time.sleep(1)
+#text = "中华人民共和国"
+#lcd.print_str12_xy(0, 1, text)
 
-time.sleep(1)
+#time.sleep(1)
+#text = "你好，蒋悦心"
+#lcd.print_str16_xy(0, 2, text)
+
+#走马灯
 text = "你好，蒋悦心"
-lcd.print_str16_xy(0, 2, text)
+x = 0
+while True:
+  time.sleep(1)
+  lcd.print_str16_xy(x, 0, text)
+  x+=1
+  if x == 127:
+    x = 0
