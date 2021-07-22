d4be43e1a90c8eecb3af21d6acbe193128cf747e
watson <jiangq@xiaopeng.com>
2021-07-22 23:07:36 +0800

    update

--- bin/lcd.py
+++ bin/lcd.py
@@ -34,13 +34,13 @@ screens.append(s1)
 
 s2 = Screen()
 text_gen = lambda: '卧龙电驱(sh000001)'
-s2.add_component(TextBox(0, 0, text_gen, 16))
+s2.add_component(TextBox(0, 0, text_gen, 12))
 text_gen = lambda: '当前价格: 12.30'
-s2.add_component(TextBox(0, 16, text_gen, 16))
+s2.add_component(TextBox(0, 16, text_gen, 12))
 text_gen = lambda: '最高价格: 19.30'
-s2.add_component(TextBox(0, 32, text_gen, 16))
+s2.add_component(TextBox(0, 32, text_gen, 12))
 text_gen = lambda: '最低价格: 19.30'
-s2.add_component(TextBox(0, 48, text_gen, 16))
+s2.add_component(TextBox(0, 48, text_gen, 12))
 screens.append(s2)
 
 screen_hold = 10 #每屏停留时间秒
@@ -48,12 +48,12 @@ idx = 0
 screen_stay_cnt = 0
 while True:
   s = screens[idx]
-  lcd.clear()
   s.render(lcd)
   time.sleep(1)
   screen_stay_cnt += 1
   if screen_stay_cnt == screen_hold:
     idx = (idx + 1) % len(screens)
+    lcd.clear()
     screen_stay_cnt = 0
 
       
