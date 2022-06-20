792370eedbf410d8c71fe59a8f3b6dc49a33a43a
jiangq <jiangq@xiaopeng.com>
2022-06-20 09:19:15 +0800

    migrate stockmon to seperate repo

--- stockmon/tests/test_hq.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-
-
-class TestModel(TestCase):
-
-    def test_fetch_all_company(self):
-        pass
--- stockmon/tests/test_main.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class TestMain(TestCase):
-    def test_foo(self):
-        print(main.foo('hello world'))
