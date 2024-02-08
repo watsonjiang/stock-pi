5369f936d976216f3e171668fe45cbbdcbd54b38
jiangq <jiangq@xiaopeng.com>
2024-02-08 10:40:41 +0800

    fix: dth11 bugfix

--- dht11.py
+++ dht11.py
@@ -91,7 +91,7 @@ async def read_device():
         for _ in range(0, 40):  # 40bit in total
             raw.append(_wait_for_dht_data())
     except:
-        pass
+        logging.exception('wait for dht data error.')
     logging.info("-----raw:{}".format(raw))
 
     rh, temp = _unpack_dht_data(raw)
